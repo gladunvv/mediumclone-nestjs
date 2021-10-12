2725c7f05545c130db0ccc8205f93560d079412f
Vladislav Gladun <bincha.1997@gmail.com>
2021-10-12 23:02:18 +0300

    tags: added tag-service

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { throws } from 'assert';
+import { TagService } from './tqg.service';
 
 @Controller('tags')
 export class TagController {
+  constructor(private readonly tagService: TagService) {}
   @Get()
-  findAll() {
-    return ['dragons', 'coffee'];
+  findAll(): string[] {
+    return this.tagService.findAll();
   }
 }
--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TagController } from './tag.controller';
+import { TagService } from './tqg.service';
 
 @Module({
   controllers: [TagController],
+  providers: [TagService],
 })
 export class TagModule {}
