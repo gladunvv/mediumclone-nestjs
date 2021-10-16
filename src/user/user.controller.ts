4f49938c48c1b3a0c451c44231a5542d7ac34b51
Vladislav Gladun <bincha.1997@gmail.com>
2021-10-17 00:53:13 +0300

    feat(user): login and createUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,34 @@
-import { Controller, Post } from '@nestjs/common';
-import { UserService } from './user.service';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from '@user/dto/createUser.dto';
+import { UserService } from '@user/user.service';
+import { LoginUserDto } from '@user/dto/login.dto';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
   @Post('users')
-  async createUser(): Promise<any> {
-    return this.userService.createUser();
+  @UsePipes(new ValidationPipe())
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async login(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.login(loginUserDto);
+    return this.userService.buildUserResponse(user);
   }
 }
