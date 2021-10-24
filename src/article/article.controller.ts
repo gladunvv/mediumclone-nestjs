e414a35f19e7bec7a28a6b5b86f2c26c7a811e90
Vladislav Gladun <bincha.1997@gmail.com>
2021-10-24 12:49:43 +0300

    feat(article): added get article by slug

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,7 +1,7 @@
 import { User } from '@user/decorators/user.decorator';
 import { AuthGuard } from '@user/guards/auth.guard';
 import { UserEntity } from '@user/user.entity';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/createArticle.dto';
 import { ArticleResponseInterface } from './types/articleResponse.interface';
@@ -20,6 +20,14 @@ export class ArticleController {
       currentUser,
       createArticleDto,
     );
+    return this.articleService.buildArticleResponse(article);
+  }
+
+  @Get(':slug')
+  async getSingleArticle(
+    @Param('slug') slug: string,
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.findBySlug(slug);
     return this.articleService.buildArticleResponse(article);
   }
 }
